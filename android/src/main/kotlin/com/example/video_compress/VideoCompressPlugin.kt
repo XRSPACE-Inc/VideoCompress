544074b275b25269683d5d92bbd019cd17e8af11
Jonny Borges <35742643+jonataslaw@users.noreply.github.com>
2024-11-04 14:32:51 -0300

    Merge pull request #286 from yaakovschectman/v1_removal
    Removes v1 Flutter Android embedding references

--- android/src/main/kotlin/com/example/video_compress/VideoCompressPlugin.kt
+++ android/src/main/kotlin/com/example/video_compress/VideoCompressPlugin.kt
@@ -18,7 +18,6 @@ import com.otaliastudios.transcoder.internal.utils.Logger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -211,12 +210,6 @@ class VideoCompressPlugin : MethodCallHandler, FlutterPlugin {
 
     companion object {
         private const val TAG = "video_compress"
-
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val instance = VideoCompressPlugin()
-            instance.init(registrar.context(), registrar.messenger())
-        }
     }
 
 }
